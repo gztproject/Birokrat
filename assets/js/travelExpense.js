025ea6c74d06a645830157d25ba70312b4fa5527
GZTproject <gasper.doljak@gmail.com>
2019-01-22 23:48:56 +0100

    Addedautomatic dsitance calculation via GoogleMapsAPI

--- assets/js/travelExpense.js
+++ assets/js/travelExpense.js
@@ -45,46 +45,124 @@ $(function() {
 
 
 var $collectionHolder;
+var $addTravelStopButton = $('<tr class="table-primary"><td colspan="4"><a id="add-travel-stop" class="btn btn-sm btn-block btn-success"><i class="fa fa-plus" aria-hidden="true"></i></a></td></tr>');
+
 
 jQuery(document).ready(function() {
     // Get the ul that holds the collection of tags
     $collectionHolder = $('tbody.travelStops');
-
-    // count the current form inputs we have (e.g. 2), use that as the new
-    // index when inserting a new item (e.g. 2)
+    
     $collectionHolder.data('index', $collectionHolder.find(':input').length);
-    addTravelStopForm($collectionHolder);
+    addTravelStopForm($collectionHolder, $addTravelStopButton, 2);
+   
+    var value = $('#travel_expense_travelStops_0_post option:contains("Škofja Loka")', $collectionHolder)[0].value;
+    $('#travel_expense_travelStops_0_post', $collectionHolder)[0].value = value;
+    $('#travel_expense_travelStops_0_distanceFromPrevious', $collectionHolder).val(0);
+    
 
-    $collectionHolder.on('change', function(e) {
+    $('#add-travel-stop').on('click', function(e) {
         // add a new tag form (see next code block)
-        addTravelStopForm($collectionHolder);
+        addTravelStopForm($collectionHolder, $addTravelStopButton, 1);        
+    });
+       
+    $('.post-Selector').on('change', function(e){
+        var index = e.currentTarget.dataset.stopIndex;        
+        autoFillDistance($collectionHolder, index);
     });
 });
 
-function addTravelStopForm($collectionHolder) {
-    // Get the data-prototype explained earlier
-    var prototype = $collectionHolder.data('prototype');
+function addTravelStopForm($collectionHolder, $addRemoveTravelStopButtons, $number) {
+    for(var i=0; i<$number;i++){
+        // Get the data-prototype explained earlier
+        var prototype = $collectionHolder.data('prototype');
 
-    // get the new index
-    var index = $collectionHolder.data('index');
+        // get the new index
+        var index = $collectionHolder.data('index');
 
-    var newForm = prototype;
-    
-    // Replace '__name__' in the prototype's HTML to
-    // instead be a number based on how many items we have
-    newForm = newForm.replace(/__name__/g, index);
-    
-    // increase the index with one for the next item
-    $collectionHolder.data('index', index + 1);
-    
+        var newForm = prototype;
+        
+        // Replace '__name__' in the prototype's HTML to
+        // instead be a number based on how many items we have
+        newForm = newForm.replace(/__name__/g, index);
+        
+        // increase the index with one for the next item
+        $collectionHolder.data('index', index + 1);
+        
 
-    // Display the form in the page in an li, before the "Add a tag" link li
-    var $newFormLi = $('<tr><td>' + $('#travel_expense_travelStops_' + index + '_stopOrder', newForm).parent().html() + 
-        '</td><td>' + $('#travel_expense_travelStops_' + index + '_post' ,newForm).parent().html() + '</td><td>' + 
-        $('#travel_expense_travelStops_' + index + '_distanceFromPrevious', newForm).parent().html()+'</td></tr>');
+        // Display the form in the page in an li, before the "Add a tag" link li
+        var $newFormLi = $('<tr class="travel-stop-tr-' + index + '"><td>' + $('#travel_expense_travelStops_' + index + '_stopOrder', newForm).parent().html() + 
+            '</td><td class="post-Selector" data-stop-index="' + index + '">' + $('#travel_expense_travelStops_' + index + '_post' ,newForm).parent().html() + '</td><td>' + 
+            $('#travel_expense_travelStops_' + index + '_distanceFromPrevious', newForm).parent().html()+'</td><td><a id="remove-travel-stop'+ index +'" class="btn btn-sm btn-block btn-danger"><i class="fa fa-minus" aria-hidden="true"></i></a></td></tr>');        
 
-    $('#travel_expense_travelStops_' + index + '_stopOrder', $newFormLi).val(index);           
-    $('#travel_expense_travelStops_' + index + '_distanceFromPrevious', $newFormLi).val((index == 0) ? 0 : Math.floor(Math.random() * 101));  
+        $('#travel_expense_travelStops_' + index + '_stopOrder', $newFormLi).val(index+1); 
+        $('#travel_expense_travelStops_' + index + '_post', $newFormLi).val('');
+        
+        $collectionHolder.append($newFormLi);
+        $collectionHolder.append($addRemoveTravelStopButtons);
 
-    $collectionHolder.append($newFormLi);
+        $('#remove-travel-stop'+index).on('click', function(e) {        
+            removeTravelStopForm($collectionHolder, index);        
+        }); 
+    }
+    $(".post-Selector").prop("onchange", null).off("change");
+
+    $('.post-Selector').on('change', function(e){
+        var index = e.currentTarget.dataset.stopIndex;        
+        autoFillDistance($collectionHolder, index);
+    });
 }
+
+function removeTravelStopForm($collectionHolder, index) {
+    // ToDo: enable removing any stop (but for the last two), automatically recalculate distances and renumber travelStopNumbers (maybe also indices).
+    if(index < 2)
+    {
+        alert("Can't delete last stop!");
+        return;
+    }
+    //$collectionHolder.data('index', index);
+    $('.travel-stop-tr-' + index, $collectionHolder).remove()
+    
+}
+
+function autoFillDistance($collectionHolder, index){
+    var rowCount = $('.post-Selector', $collectionHolder).length;
+    if(rowCount > (parseInt(index) + 1)){
+        var origin = $('#travel_expense_travelStops_' + index + '_post option:selected', $collectionHolder)[0].text;
+        var destination = $('#travel_expense_travelStops_' + (parseInt(index) + 1) + '_post option:selected', $collectionHolder)[0].text;    
+        getDistance(origin, destination, function(distance){
+            $('#travel_expense_travelStops_' + (parseInt(index)+1) + '_distanceFromPrevious', $collectionHolder).val((index == 0) ? 0 : distance/1000);   
+        });  
+    }    
+    if (index > 0)
+    {
+        var origin = $('#travel_expense_travelStops_' + (parseInt(index) - 1) + '_post option:selected', $collectionHolder)[0].text;
+        var destination = $('#travel_expense_travelStops_' + index + '_post option:selected', $collectionHolder)[0].text;
+
+        getDistance(origin, destination, function(distance){
+            $('#travel_expense_travelStops_' + index + '_distanceFromPrevious', $collectionHolder).val((index == 0) ? 0 : distance/1000);   
+        });             
+    }
+}
+
+function getDistance(origin, destination, callback){
+    var service = new google.maps.DistanceMatrixService();
+    
+    service.getDistanceMatrix(
+    {
+        origins: [origin],
+        destinations: [destination],
+        travelMode: 'DRIVING',
+        unitSystem: google.maps.UnitSystem.METRIC,
+        avoidHighways: false,
+        avoidTolls: false
+    }, 
+    function (response, status) {
+        if (status !== 'OK') {
+            alert('Error was: ' + status);
+        } 
+        else {             
+            callback(response.rows[0].elements[0].distance.value);           
+        }
+    });
+}
+
--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
