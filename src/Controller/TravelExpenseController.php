1d85a67b7a05c43f3d0a29d012031c9cfd37bd76
GZTproject <gasper.doljak@gmail.com>
2019-02-21 02:01:54 +0100

    ToDo reminder

--- src/Controller/TravelExpenseController.php
+++ src/Controller/TravelExpenseController.php
@@ -44,6 +44,7 @@ class TravelExpenseController extends AbstractController
     		$te->setState($state);
     		
     		$te->setEmployee($this->get('security.token_storage')->getToken()->getUser());
+    		//ToDo: Get rate from organizationSettings
     		$te->setRate(0.37);
     		$te->calculateTotalDistance(); 
     		
