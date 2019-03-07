389a5ce2ddce026cfba8e31daaeef9130a9e350e
GZTproject <gasper.doljak@gmail.com>
2019-03-07 01:50:27 +0100

    Some tryouts with TCPDF

--- src/Controller/InvoiceController.php
+++ src/Controller/InvoiceController.php
@@ -12,6 +12,7 @@ use App\Repository\InvoiceRepository;
 use Symfony\Component\Validator\Constraints\DateTime;
 use App\Entity\Invoice\InvoiceNumberFactory;
 use App\Entity\Konto\Konto;
+use WhiteOctober\TCPDFBundle\Controller\TCPDFController;
 
 class InvoiceController extends AbstractController
 {    
@@ -118,6 +119,79 @@ class InvoiceController extends AbstractController
     	$entityManager->flush();
     	
     	return $this->redirectToRoute('invoice_index');
+    }
+    
+    /**
+     * @Route("/dashboard/invoice/pdf/{id<[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}>}", methods={"GET"}, name="invoice_pdf")
+     */
+    public function generatePdf(Invoice $invoice, TCPDFController $tcpdf): Response
+    {
+    	$pdf = $tcpdf->create();
+    	/**
+    	 * Creates an example PDF TEST document using TCPDF
+    	 * @package com.tecnick.tcpdf
+    	 * @abstract TCPDF - Example: Removing Header and Footer
+    	 * @author Nicola Asuni
+    	 * @since 2008-03-04
+    	 */    	
+    	    	
+    	// set document information
+    	$pdf->SetCreator(PDF_CREATOR);
+    	$pdf->SetAuthor('Nicola Asuni');
+    	$pdf->SetTitle('TCPDF Example 002');
+    	$pdf->SetSubject('TCPDF Tutorial');
+    	$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+    	
+    	// remove default header/footer
+    	$pdf->setPrintHeader(false);
+    	$pdf->setPrintFooter(false);
+    	
+    	// set default monospaced font
+    	$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+    	
+    	// set margins
+    	$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+    	
+    	// set auto page breaks
+    	$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+    	
+    	// set image scale factor
+    	$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+    	
+    	// set some language-dependent strings (optional)
+    	if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
+    		require_once(dirname(__FILE__).'/lang/eng.php');
+    		$pdf->setLanguageArray($l);
+    	}
+    	
+    	// ---------------------------------------------------------
+    	
+    	// set font
+    	$pdf->SetFont('times', 'BI', 20);
+    	
+    	// add a page
+    	$pdf->AddPage();
+    	
+    	// set some text to print
+    	$n = $invoice->getNumber();
+    	$txt = <<<EOD
+		Invoice $n
+
+		Default page header and footer are disabled using setPrintHeader() and setPrintFooter() methods.
+		EOD;
+    	
+    	// print a block of text using Write()
+    	$pdf->Write(0, $txt, '', 0, 'C', true, 0, false, false, 0);
+    	
+    	// ---------------------------------------------------------
+    	
+    	//Close and output PDF document
+    	$pdf->Output('example_002.pdf', 'I');
+    	
+    	//============================================================+
+    	// END OF FILE
+    	//============================================================+    	
+    	
     }
     
     /**
