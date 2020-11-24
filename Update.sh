ee29fd501a9603e9f7595a04f803eb509f4e5f77
GZTproject <gasper.doljak@gmail.com>
2020-11-24 19:09:25 +0100

    git reset in update script

--- Update.sh
+++ Update.sh
@@ -1,11 +1,12 @@
 #/bin/bash
 
 # Pull master branch
-git reset --hard ??
+git reset --hard
 git pull origin master
 
 #install new dependencies
 rm -rf vendor/
+rm -rf node-modules/
  
 composer install --no-dev --optimize-autoloader
 
@@ -13,6 +14,8 @@ php ./bin/console cache:clear --env=prod
 
 #set Environment variables
 composer dump-env prod
+
+
 
 #Build new .js and .css
 yarn install
