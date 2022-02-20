e4694f5183faf079ded1a316e3d43731a8fa2bc9
GZTproject <gasper.doljak@gmail.com>
2022-02-20 16:11:28 +0100

    Updated recipe

--- webpack.config.js
+++ webpack.config.js
@@ -1,81 +1,75 @@
-// webpack.config.js
-const Dotenv = require('dotenv-webpack');
-var Encore = require('@symfony/webpack-encore');
+const Encore = require('@symfony/webpack-encore');
 
-Encore    
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    // directory where compiled assets will be stored
     .setOutputPath('public/build/')
+    // public path used by the web server to access the output path
     .setPublicPath('/build')
-    .cleanupOutputBeforeBuild()
-    .autoProvidejQuery()
-    .autoProvideVariables({
-        "window.Bloodhound": require.resolve('bloodhound-js'),
-        "jQuery.tagsinput": "bootstrap-tagsinput"
-    })
-    .enableSassLoader()
-    .enableVersioning()
-    .addEntry('js/app', './assets/js/app.js')
-    .addEntry('js/user/admin', './assets/js/user/admin.js')
-    .addEntry('js/user/showUser', './assets/js/user/showUser.js')
-    .addEntry('js/dashboard', './assets/js/dashboard.js')
-    .addEntry('js/search', './assets/js/search.js')
-    .addEntry('js/codesheets', './assets/js/codesheets.js')
-    
-    //TravelExpense
-    .addEntry('js/travelExpense/index', './assets/js/travelExpense/index.js')
-    .addEntry('js/travelExpense/new', './assets/js/travelExpense/new.js')
-    .addEntry('js/travelExpense/filter', './assets/js/travelExpense/filter.js')
+    // only needed for CDN's or sub-directory deploy
+    //.setManifestKeyPrefix('build/')
 
-    //LunchExpense
-    .addEntry('js/lunchExpense/index', './assets/js/lunchExpense/index.js')
-    .addEntry('js/lunchExpense/filter', './assets/js/lunchExpense/filter.js')
+    /*
+     * ENTRY CONFIG
+     *
+     * Each entry will result in one JavaScript file (e.g. app.js)
+     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+     */
+    .addEntry('app', './assets/app.js')
 
-    //Invoice
-    .addEntry('js/invoice/new', './assets/js/invoice/new.js')
-    .addEntry('js/invoice/list', './assets/js/invoice/list.js')
-    .addEntry('js/invoice/view', './assets/js/invoice/view.js')
+    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
+    .enableStimulusBridge('./assets/controllers.json')
 
-    //IncomingInvoice
-    .addEntry('js/incomingInvoice/new', './assets/js/incomingInvoice/new.js')
-    .addEntry('js/incomingInvoice/list', './assets/js/incomingInvoice/list.js')
-    
-    //Organization
-    .addEntry('js/organization/new', './assets/js/organization/new.js')
-    .addEntry('js/organization/show', './assets/js/organization/show.js')
-
-    //Transaction
-    .addEntry('js/transaction/new', './assets/js/transaction/new.js')
-
-    //Geography
-    .addEntry('js/geography/country/show', './assets/js/geography/country/show.js')
-
-    //Common
-    //  ->filters
-    .addEntry('js/common/filters/dateOrgFilter', './assets/js/common/filters/dateOrgFilter.js')
-
-    //POC
-    .addEntry('js/poc.typeahead', './assets/js/poc.typeahead.js')
-
-    
-    //Styles
-    .addStyleEntry('css/app', ['./assets/scss/app.scss'])
-    .addStyleEntry('css/copyleft', ['./assets/css/copyleft.css'])
-    .addStyleEntry('css/admin', ['./assets/scss/admin.scss'])
-    .addStyleEntry('css/dashboard', ['./assets/scss/dashboard.scss'])
-    .addStyleEntry('css/invoice', ['./assets/scss/invoice.scss'])
-    .addStyleEntry('css/report', ['./assets/scss/report.scss'])    
-
-    //plugins
-
-    .addPlugin(new Dotenv({path: './.env.local'}))
-
-    .configureBabel(function(babelConfig) {
-    })
-
+    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
-    .enableSourceMaps(!Encore.isProduction())
+
+    // will require an extra script tag for runtime.js
+    // but, you probably want this, unless you're building a single-page app
     .enableSingleRuntimeChunk()
+
+    /*
+     * FEATURE CONFIG
+     *
+     * Enable & configure other features below. For a full
+     * list of features, see:
+     * https://symfony.com/doc/current/frontend.html#adding-more-features
+     */
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    // enables hashed filenames (e.g. app.abc123.css)
+    .enableVersioning(Encore.isProduction())
+
+    .configureBabel((config) => {
+        config.plugins.push('@babel/plugin-proposal-class-properties');
+    })
+
+    // enables @babel/preset-env polyfills
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    })
+
+    // enables Sass/SCSS support
+    //.enableSassLoader()
+
+    // uncomment if you use TypeScript
+    //.enableTypeScriptLoader()
+
+    // uncomment if you use React
+    //.enableReactPreset()
+
+    // uncomment to get integrity="..." attributes on your script & link tags
+    // requires WebpackEncoreBundle 1.4 or higher
+    //.enableIntegrityHashes(Encore.isProduction())
+
+    // uncomment if you're having problems with a jQuery plugin
+    //.autoProvidejQuery()
 ;
 
-module.exports = Encore.getWebpackConfig();    
-
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+module.exports = Encore.getWebpackConfig();
